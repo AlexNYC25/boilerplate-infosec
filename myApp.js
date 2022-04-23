ebbabc84b184fc4de2c6f169b91e892b66deb433
Alexis Montes <montes.alexis25@gmail.com>
2022-04-23 01:56:16 -0400

    add https proction for app

--- myApp.js
+++ myApp.js
@@ -58,6 +58,10 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({
+  maxAge: 90*24*60*60, 
+  force: true
+}));
 
 app.use('/_api', api);
 app.get("/", function (request, response) {
