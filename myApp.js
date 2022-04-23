459d31d7329796d6ec7736d4dde4aea1c183d6f2
Alexis Montes <montes.alexis25@gmail.com>
2022-04-23 02:21:56 -0400

    added helmet generall protection

--- myApp.js
+++ myApp.js
@@ -53,16 +53,18 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 // helmet middleware
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({
-  maxAge: 90*24*60*60, 
-  force: true
-}));
-app.use(helmet.dnsPrefetchControl());
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy({
   directives: {
